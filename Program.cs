138dc6a6260029ef5c0fec986eb2ffedace410de
Amberssone <108621175+Amberssone@users.noreply.github.com>
2022-07-17 21:47:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// 10
+
+string number = Console.ReadLine();
+
+if (number.Length() < 2) {
+    Console.WriteLine("doesn't exist");
+}
+else {
+    Console.WriteLine(number[3]);
+}
+
+// 15.
+
+string day = Console.ReadLine();
+
+if (day == "6" || day == "7") {
+    Console.WriteLine("yes");
+}
+else {
+    Console.WriteLine("no");
+}
+
+// 13.
+
+string number = Console.ReadLine();
+
+if (number.Length() < 3) {
+    Console.WriteLine("doesn't exist");
+}
+else {
+    Console.WriteLine(number[2]);
+}
+
